5a2f49b7dbf37b0b5928c924a5874b959c121d0a
Audrey Pujol <audreypujol@localhost.local>
2021-07-26 10:35:56 -0300

    Get /videos e Get /videos/ID

--- app.py
+++ app.py
@@ -14,3 +14,16 @@ def videos():
     return (todos_string)
 
 
+@app.route('/videos/<video_id>')
+def show_video_information(video_id):
+    con = sq3.connect('frttt.db')
+    cur = con.cursor()
+    todos = list(cur.execute('Select * from videos where ID = {}'.format(video_id)))
+    if len (todos) == 0:
+        return 'Ops, não consegui encontrar o vídeo que você está procurando!'
+    else: 
+        todos_video_id = jsn.dumps(todos[0])
+        return (todos_video_id)
+
+
+
