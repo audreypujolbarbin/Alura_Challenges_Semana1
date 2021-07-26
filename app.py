81e4cc7f9c1010b26f6e49b4d3a4b7faffdec1c2
Audrey Pujol <audreypujol@localhost.local>
2021-07-26 14:14:54 -0300

    Alteração de vídeo

--- app.py
+++ app.py
@@ -58,18 +58,33 @@ def novo_video():
         return show_video_information(novo_id)
 
 def validate_payload(title, description, url): 
+    if not validate_payload_title(title):
+        return False
+    if not validate_payload_description(description):
+        return False
+    if not validate_payload_url(url):
+        return False
+    return True
+
+def validate_payload_title(title):
     if not isinstance (title, str):
         return False
     if len (title) == 0:
         return False
     if len (title) >= 50:
         return False
+    return True
+
+def validate_payload_description(description):
     if not isinstance (description, str):
         return False
     if len (description) == 0:
         return False
     if len (description) >= 150:
         return False
+    return True 
+
+def validate_payload_url(url):
     if not isinstance (url, str):
         return False
     if len (url) == 0:
@@ -77,3 +92,24 @@ def validate_payload(title, description, url):
     if len (url) >= 200:
         return False
     return True
+
+
+
+
+
+@app.route('/videos/<video_id>', methods=['PUT'])
+def atualizar_video(video_id):
+    con = sq3.connect('frttt.db')
+    cur = con.cursor()
+    if 'Título' in request.form:
+        if validate_payload_title (request.form['Título']):
+            cur.execute("UPDATE videos SET Titulo = '{}' WHERE ID = {}".format(request.form['Título'], video_id))
+    if 'Descrição' in request.form:
+        if validate_payload_description (request.form['Descrição']):
+            cur.execute("UPDATE videos SET Descricao = '{}' WHERE ID = {}".format(request.form['Descrição'], video_id))
+    if 'URL' in request.form:
+        if validate_payload_url (request.form['URL']):
+            cur.execute("UPDATE videos SET URL = '{}' WHERE ID = {}".format(request.form['URL'], video_id))
+    con.commit()
+    con.close ()
+    return show_video_information(video_id)
